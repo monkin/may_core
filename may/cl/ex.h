c4ce98da47b497f06f390284f25a68e3c9df2603
Andrey Monkin <andrey@mpc>
2011-10-23 20:07:04 +0400

    Add some OpenCL functions.

--- may/cl/ex.h
+++ may/cl/ex.h
@@ -43,8 +43,8 @@ mcl_ex_t mcl_var(heap_t h, mcl_ex_t);
 mcl_ex_t mcl_arg(heap_t h, mclt_t tp, mcl_arg_t *);
 mcl_ex_t mcl_const(heap_t h, mclt_t tp, const void *val);
 mcl_ex_t mcl_seq(heap_t h, size_t n, ...);
-mcl_ex_t mcl_if(heap_t h, mcl_ex_t, mcl_ex_t);
-mcl_ex_t mcl_unless(heap_t h, mcl_ex_t, mcl_ex_t);
+mcl_ex_t mcl_if(heap_t h, mcl_ex_t, mcl_ex_t, mcl_ex_t);
+mcl_ex_t mcl_unless(heap_t h, mcl_ex_t, mcl_ex_t, mcl_ex_t);
 mcl_ex_t mcl_for(heap_t h, mcl_ex_t var_init, mcl_ex_t var_cond, mcl_ex_t var_inc, mcl_ex_t ex);
 mcl_ex_t mcl_cast(heap_t h, mclt_t, mcl_ex_t);
 

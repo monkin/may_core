483b7c0dc2bbaa77fe65ddbbfc1c3c49fb1902bf
Andrey Monkin <monkin.andrey@gmail.com>
2011-12-24 04:38:16 +0400

    mcl_ex: append operations sequence

--- may/cl/ex/seq.c
+++ may/cl/ex/seq.c
@@ -2,17 +2,44 @@
 #	error This file should be included from /may/cl/ex.c
 #endif
 
-void seq_push_arguments(void *data, void (*push_fn)(void *, mcl_arg_t), void *dt) {
+#include <stdarg.h>
 
+typedef struct {
+	size_t ex_count;
+	mcl_ex_t *ex;
+	mcl_ex_s self;
+} seq_data_s;
+
+typedef seq_data_s *seq_data_t;
+
+void seq_push_arguments(void *data, void (*push_fn)(void *, mcl_arg_t), void *dt) {
+	seq_data_t sd = data;
+	size_t i;
+	for(i=0; i<sd->ex_count; i++)
+		mcl_push_arguments(sd->ex[i], push_fn, dt);
 }
 void seq_global_source(void *data, map_t m, ios_t s) {
-
+	seq_data_t sd = data;
+	size_t i;
+	for(i=0; i<sd->ex_count; i++)
+		mcl_global_source(sd->ex[i], m, s);
 }
 void seq_local_source(void *data, map_t m, ios_t s) {
-
+	seq_data_t sd = data;
+	size_t i;
+	for(i=0; i<sd->ex_count; i++)
+		mcl_local_source(sd->ex[i], m, s);
 }
-void seq_value_source(void *data, ios_t s) {
 
+void seq_value_source(void *data, ios_t s) {
+	seq_data_t sd = data;
+	size_t i;
+	ios_write(s, "{\n", 2);
+	for(i=0; i<sd->ex_count; i++) {
+		mcl_value_source(sd->ex[i], s);
+		ios_write(s, ";\n", 2);
+	}
+	ios_write(s, "}", 1);
 }
 
 static mcl_ex_vtable_s seq_vtable = {
@@ -21,3 +48,20 @@ static mcl_ex_vtable_s seq_vtable = {
 	seq_local_source,
 	seq_value_source
 };
+
+mcl_ex_t mcl_seq(heap_t h, size_t n, ...) {
+	size_t i;
+	va_list ap;
+	seq_data_t r = heap_alloc(h, sizeof(seq_data_s));
+	r->ex_count = n;
+	r->ex = heap_alloc(h, sizeof(mcl_ex_t[n]));
+	va_start(ap, n);
+	for(i=0; i<n; i++)
+		r->ex[i] = va_arg(ap, mcl_ex_t);
+	va_end(ap);
+	r->self.vtable = &seq_vtable;
+	r->self.return_type = MCLT_VOID;
+	r->self.data = r;
+	return &r->self;
+}
+

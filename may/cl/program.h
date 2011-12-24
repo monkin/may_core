b74040a91d563f669e98f5025b9d6381a6633d7f
Andrey Monkin <monkin.andrey@gmail.com>
2011-12-24 08:21:20 +0400

    mcl_prog: interface complete

--- may/cl/program.h
+++ may/cl/program.h
@@ -1,21 +1,32 @@
 
-#ifndef MAY_MCLPROGRAM_H
-#define MAY_MCLPROGRAM_H
+#ifndef MAY_mcl_progRAM_H
+#define MAY_mcl_progRAM_H
 
 #include "mcl.h"
 #include "ex.h"
+#include "<CL/cl.h>"
 
 typedef struct {
 	cl_program program;
 	map_t variables;
-} mclprog_s;
+} mcl_prog_s;
 
-typedef mclprog_s *mclprog_t;
+typedef struct {
+	cl_kernel kernel;
+	map_t variables;
+} mcl_kernel_s;
 
-mclprog_t mclprog_create(cl_context, mcl_ex_t);
-void mclprog_build(mclprog_t, void (*callback)(void *), void *callback_data);
-mclprog_t mclprog_delete(mclprog_t);
+typedef mcl_prog_s *mcl_prog_t;
+typedef mcl_kernel_s *mcl_kernel_t;
 
+mcl_prog_t mcl_prog_create(cl_context, mcl_ex_t);
+mcl_prog_t mcl_prog_delete(mcl_prog_t);
 
-#endif /* MAY_MCLPROGRAM_H */
+void mcl_prog_build(mcl_prog_t, void (*callback)(void *), void *callback_data);
+
+mcl_kernel_t mcl_kernel_create(mcl_prog_t);
+mcl_kernel_t mcl_kernel_delete(mcl_kernel_t);
+void mcl_kernel_arg(mcl_kernel_t, mcl_arg_t, size_t arg_size, const void *arg_value);
+
+#endif /* MAY_mcl_progRAM_H */
 

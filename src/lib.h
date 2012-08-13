bc90f04d514bf51c38788e73e2080ff07dc6bd00
Andrey Monkin <monkin.andrey@gmail.com>
2012-08-13 16:37:58 +0400

    Append all core includes to lib.h

--- src/lib.h
+++ src/lib.h
@@ -2,6 +2,22 @@
 #ifndef MAY_MAYLIB_H
 #define MAY_MAYLIB_H
 
+#include "err.h"
+#include "event.h"
+#include "floader.h"
+#include "heap.h"
+#include "json.h"
+#include "map.h"
+#include "mem.h"
+#include "mutex.h"
+#include "parser.h"
+#include "stream.h"
+#include "str.h"
+#include "syntree.h"
+#include "tar.h"
+#include "test.h"
+#include "utf.h"
+
 void may_core_init();
 
 #endif /* MAY_MAYLIB_H */

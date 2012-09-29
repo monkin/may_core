5a651531d168269a36fec9165803f12e4f484bfd
Andrey Monkin <monkin.andrey@gmail.com>
2012-09-29 23:44:03 +0400

    Array fixes

--- src/arr.c
+++ src/arr.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 arr_t arr_create(heap_t h, size_t item_size, size_t length) {
-	arr_t r = heap_alloc(h, sizeof(arr_s));
+	arr_t r = heap_alloc(h, sizeof(arr_s) + (item_size * length));
 	r->item_size = item_size;
 	r->length = length;
-	r->data = heap_alloc(h, item_size*length);
+	r->data = ((char *) r) + sizeof(arr_s);
 	return r;
 }
 
